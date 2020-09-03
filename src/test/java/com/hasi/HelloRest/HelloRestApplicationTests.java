7523d8a24733831778ca5e4be2d83d608b8ac25e
hasihime <46011077+hasihime@users.noreply.github.com>
2020-09-03 22:59:38 +0900

    make helloController

---
+++ src/test/java/com/hasi/HelloRest/HelloRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasi.HelloRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
