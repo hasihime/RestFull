4592881951b524e94d8fba9fb01e199bde3c5931
hasihime <46011077+hasihime@users.noreply.github.com>
2020-09-06 23:07:28 +0900

    class date add

--- src/main/java/com/hasi/HelloRest/Controller/HelloController.java
+++ src/main/java/com/hasi/HelloRest/Controller/HelloController.java
@@ -1,13 +1,29 @@
 package com.hasi.HelloRest.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	@GetMapping(path = "/hello")
-	public String GetHello() {
+	public String GetHello(Model model) {
+		Data data=new Data("hi", "27");
+		model.addAttribute("data",data);
 		return "Hello";
 	}
 
+	
+	//data Ãß°¡
+	class Data{
+		String name;
+		String age;
+		public Data(String name, String age) {
+			super();
+			this.name = name;
+			this.age = age;
+		}
+		
+
+	}
 }
